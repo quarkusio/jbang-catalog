793dac5fa2e6be20c5f7c68cd5ea7db4aabe0303
George Gastaldi <gegastaldi@gmail.com>
2022-02-21 18:20:55 -0300

    Publish compatibility data per extension

--- catalog_publish.java
+++ catalog_publish.java
@@ -2,7 +2,7 @@
 //DEPS info.picocli:picocli:4.6.1
 //DEPS io.quarkus:quarkus-devtools-registry-client:2.7.1.Final
 //JAVA_OPTIONS "-Djava.util.logging.SimpleFormatter.format=%1$s [%4$s] %5$s%6$s%n"
-//JAVA 11
+//JAVA 17
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,17 +12,23 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import io.quarkus.registry.catalog.CatalogMapperHelper;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -30,6 +36,7 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.jboss.logging.Logger;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -54,7 +61,7 @@ class catalog_publish implements Callable<Integer> {
             "--token" }, description = "The token to use when authenticating to the admin endpoint", defaultValue = "${REGISTRY_TOKEN}")
     String token;
 
-    @Option(names = {"-a", "--all"}, description = "Publish all versions? If false, just the latest is published")
+    @Option(names = { "-a", "--all" }, description = "Publish all versions? If false, just the latest is published")
     boolean all;
 
     private final ObjectMapper yamlMapper;
@@ -155,16 +162,25 @@ class catalog_publish implements Callable<Integer> {
             String artifactId = tree.get("artifact-id").asText();
             for (JsonNode node : tree.withArray("versions")) {
                 String version;
+                List<String> compatibleWithQuarkusVersions;
                 if (node.isObject()) {
                     version = node.fieldNames().next();
+                    compatibleWithQuarkusVersions = StreamSupport.stream(
+                                    node.path(version).withArray("compatible-with-quarkus-core").spliterator(), false)
+                            .map(JsonNode::asText)
+                            .collect(Collectors.toList());
                 } else {
                     version = node.asText();
+                    compatibleWithQuarkusVersions = Collections.emptyList();
                 }
                 // Get Extension YAML
                 byte[] jsonExtension = readExtension(repository, groupId, artifactId, version);
                 // Publish
                 log.infof("Publishing %s:%s:%s", groupId, artifactId, version);
                 publishExtension(jsonExtension);
+                if (compatibleWithQuarkusVersions.size() > 0) {
+                    publishCompatibility(groupId, artifactId, version, compatibleWithQuarkusVersions);
+                }
                 if (!all) {
                     // Just publish the first one
                     break;
@@ -257,6 +273,37 @@ class catalog_publish implements Callable<Integer> {
                 }
             }
         }
+    }
+
+    private void publishCompatibility(String groupId, String artifactId, String version,
+            List<String> compatibleWithQuarkusVersions) throws IOException {
+        try (final CloseableHttpClient httpClient = createHttpClient()) {
+            HttpPost post = new HttpPost(registryURL.resolve("/admin/v1/extension/compat"));
+            post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+            if (token != null) {
+                post.setHeader("Token", token);
+            }
+            for (String quarkusCore : compatibleWithQuarkusVersions) {
+                List<NameValuePair> params = List.of(
+                        new BasicNameValuePair("groupId", groupId),
+                        new BasicNameValuePair("artifactId", artifactId),
+                        new BasicNameValuePair("version", version),
+                        new BasicNameValuePair("quarkusCore", version),
+                        new BasicNameValuePair("compatible", "true")
+                );
+                post.setEntity(new UrlEncodedFormEntity(params));
+                try (CloseableHttpResponse response = httpClient.execute(post)) {
+                    StatusLine statusLine = response.getStatusLine();
+                    if (statusLine.getStatusCode() != HttpURLConnection.HTTP_ACCEPTED) {
+                        throw new IOException(statusLine.getStatusCode() + " -> " + statusLine.getReasonPhrase());
+                    } else {
+                        log.infof("Extension %s:%s:%s is now marked as compatible with Quarkus %s", groupId, artifactId,
+                                version, quarkusCore);
+                    }
+                }
+            }
+        }
+
     }
 
     private CloseableHttpClient createHttpClient() {
